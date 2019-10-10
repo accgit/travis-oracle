99476f0ee0dd6213f33706339b46179cd93e38c4
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-10-10 21:41:46 +0200

    Test

--- install.client.sh
+++ install.client.sh
@@ -6,7 +6,7 @@ dpkg -i oracle-instantclient12.2-basic_12.2.0.1.0-2_amd64.deb
 dpkg -i oracle-instantclient12.2-devel_12.2.0.1.0-2_amd64.deb
 dpkg -i oracle-instantclient12.2-sqlplus_12.2.0.1.0-2_amd64.deb
 
-ENV LD_LIBRARY_PATH /usr/lib/oracle/12.2/client64/lib/
-ENV ORACLE_HOME /usr/lib/oracle/12.2/client64/lib/
+ENV LD_LIBRARY_PATH="/usr/lib/oracle/12.2/client64/lib/"
+ENV ORACLE_HOME="/usr/lib/oracle/12.2/client64/lib/"
 
-echo 'instantclient,/usr/lib/oracle/12.2/client64/lib' | pecl install oci8 && docker-php-ext-enable oci8
+echo 'instantclient,/usr/lib/oracle/12.2/client64/lib' | pecl install oci8 && php-ext-enable oci8
