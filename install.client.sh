0a798b04844da9a97ea5a45f3d7800bc46886cb5
accgit <zdenek.papucik@gmail.com>
2019-10-11 12:03:41 +0200

    Test

--- install.client.sh
+++ install.client.sh
@@ -1,20 +1,5 @@
 #!/bin/sh -e
 
-mkdir /opt/oracle
+wget https://raw.githubusercontent.com/accgit/travis-oracle/master/oracle/client/oracle-instantclient12.2-basic-12.2.0.1.0-1.x86_64.rpm
+wget https://raw.githubusercontent.com/accgit/travis-oracle/master/oracle/client/oracle-instantclient12.2-devel-12.2.0.1.0-1.x86_64.rpm
 
-cd /opt/oracle
-unzip ./oracle/instantclient-basic-linux.x64-12.1.0.2.0.zip
-unzip ./oracle/instantclient-sdk-linux.x64-12.1.0.2.0.zip
-
-ln -s /opt/oracle/instantclient_12_1/libclntsh.so.12.1 /opt/oracle/instantclient_12_1/libclntsh.so
-ln -s /opt/oracle/instantclient_12_1/libocci.so.12.1 /opt/oracle/instantclient_12_1/libocci.so
-
-echo /opt/oracle/instantclient_12_1 > /etc/ld.so.conf.d/oracle-instantclient
-
-pecl install oci8
-
-echo instantclient,/opt/oracle/instantclient_12_1
-echo "extension = oci8.so" >> /etc/php/7.1/fpm/php.ini
-echo "extension = oci8.so" >> /etc/php/7.1/cli/php.ini
-
-service php7.1-fpm restart
